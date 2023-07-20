a3220b1fe3bcf2b06ec23589aad29f322820c7de
Roman <125289934+midnightwolf33@users.noreply.github.com>
2023-07-20 15:02:05 +0300

    Add files via upload

---
+++ JavaSemOOPHW2.java
@@ -0,0 +1,29 @@
+package Seminar_1;
+
+public class dz_2 {
+
+    public static void main(String[] args) {
+        int limit = 1000;
+
+        System.out.println("Простые числа от 1 до " + limit + ":");
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
